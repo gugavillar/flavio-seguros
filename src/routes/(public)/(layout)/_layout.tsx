94a2c0f1b1637bed4008ad365250f54fb33db73d
Guga Villar <grsv.21@gmail.com>
2026-01-31 16:57:38 -0300

    :sparkles: Cookie consent

--- src/routes/(public)/(layout)/_layout.tsx
+++ src/routes/(public)/(layout)/_layout.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { ClientOnly, createFileRoute, Outlet } from '@tanstack/react-router'
+import CookieConsent from 'react-cookie-consent'
 
 import { Footer, Navbar } from '@/components/public'
 
@@ -14,6 +15,28 @@ function PublicLayoutComponent() {
 				<Outlet />
 			</main>
 			<Footer />
+			<ClientOnly>
+				<CookieConsent
+					buttonClasses='inline-flex! cursor-pointer! items-center! justify-center! gap-2! whitespace-nowrap! rounded-md! bg-transparent! border! border-gray-200! px-4! py-2! font-medium! text-sm! text-white! ring-offset-primary! transition-all! duration-500! hover:bg-slate-50/20! focus-visible:outline-none! focus-visible:ring-2! focus-visible:ring-ring! focus-visible:ring-offset-2!'
+					buttonText='Aceitar'
+					containerClasses='justify-center! items-center! bg-primary-gradient! max-lg:top-0! max-lg:bottom-auto!'
+					contentClasses='container mx-auto max-w-[1425px] m-0! px-4 py-2'
+					cookieName='cookieConsent'
+					location='bottom'
+				>
+					Usamos cookies essenciais e de análise para garantir o funcionamento do site e melhorar sua experiência. Saiba
+					mais na{' '}
+					<a
+						className='underline underline-offset-4'
+						href='/docs/politica-de-cookies.pdf'
+						rel='noopener noreferrer'
+						target='_blank'
+					>
+						Política de Cookies
+					</a>
+					.
+				</CookieConsent>
+			</ClientOnly>
 		</>
 	)
 }
