c67c268feb0610e9c12b6aab5b1825f82ff664c8
Guga Villar <grsv.21@gmail.com>
2026-04-25 13:08:00 -0300

    ⬆️ Upgrade dependencies

--- src/components/core/Accordion/Accordion.tsx
+++ src/components/core/Accordion/Accordion.tsx
@@ -48,4 +48,4 @@ const AccordionContent = forwardRef<
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger }
