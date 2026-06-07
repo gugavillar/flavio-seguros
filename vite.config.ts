cb0410b0c47a714e59e19a8effa4c4d1f97d3cb1
Guga Villar <grsv.21@gmail.com>
2026-06-07 13:31:30 -0300

    🐛 Error aws services

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ const config = defineConfig({
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
+	ssr: {
+		noExternal: ['@aws-crypto/crc32', /^@aws-sdk\//],
+	},
 	plugins: [
 		devtools(),
 		nitro(),
